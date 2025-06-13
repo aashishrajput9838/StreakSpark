2fc22cc99c50e7e911945e60f84eec43b304a275
aashishrajput9838 <aashishrajput9838@gmail.com>
2025-06-14 00:10:23 +0530

    feat: Make StreakSpark logo in Navbar redirect to home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-xl font-bold">StreakSpark</div>
+        <Link to="/" className="text-xl font-bold">StreakSpark</Link>
         <div className="space-x-4">
           {user ? (
             <>
